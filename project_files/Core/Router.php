d7389746f4da739fd4aa291a5bc3d8681b8b39ac
KostasEreksonas <k.ereksonas@gmail.com>
2023-03-06 12:00:11 +0200

    Added route matching

--- project_files/Core/Router.php
+++ project_files/Core/Router.php
@@ -11,31 +11,35 @@ class Router
     protected array $routes = []; // Associative array of routes (the routing table)
     protected array $params = []; // Parameters from the matched route
 
-    public function add(string $route, array $params): void // Add a route to the routing table
+    public function add(string $route, array $params = []): void // Add a route to the routing table
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Add start and end delimiters, and case insesitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
     public function match(string $url): bool // Match the route to the routes in the routing table, setting the $params property if a route is found
     {
-        /*foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                // Get named capture group values
+                //$params = [];
+
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+
                 $this->params = $params;
                 return true;
             }
-        }*/
-        $regexp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-        if (preg_match($regexp, $url, $matches)) {
-            // Get named capture group values
-            $params = [];
-
-            foreach ($matches as $key => $match) {
-                if (is_string($key)) {
-                    $params[$key] = $match;
-                }
-            }
-
-            $this->params = $params;
-            return true;
         }
         return false;
     }
--- project_files/public/index.php
+++ project_files/public/index.php
@@ -24,7 +24,8 @@ $router = new Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
-
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
 // Display the routing table
 /**echo '<pre>';
 *var_dump($router->getRoutes());
