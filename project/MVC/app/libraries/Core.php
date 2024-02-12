7c46f19bfa0c580426cb132eacc9279b342aa01d
KostasEreksonas <k.ereksonas@gmail.com>
2024-02-12 22:27:39 +0200

    Added folder structure

---
+++ project/MVC/app/libraries/Core.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+     * App Core Class
+     * Creates URL & loads core controller
+     * URL FORMAT - /controller/method/parameters
+     */
+    class Core {
+        protected $currentController = 'Pages';
+    }
