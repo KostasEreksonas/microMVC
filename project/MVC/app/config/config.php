7cdacee980d35d7293cc8a1145f09da2051aba66
KostasEreksonas <k.ereksonas@gmail.com>
2024-02-14 22:07:49 +0200

    Created MVC core functionality

---
+++ project/MVC/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DBU_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'MVC');
+
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost/MVC');
+    // Site name
+    define('SITENAME', 'microMVC');
--- project/MVC/app/controllers/Pages.php
+++ project/MVC/app/controllers/Pages.php
@@ -1,12 +1,20 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct(){
-
+            $this->postModel = $this->model('Post');
         }
         public function index(){
-
+            $posts = $this->postModel->getPosts();
+            $data = [
+                'title' => 'Welcome',
+                'posts' => $posts
+            ];
+            $this->view('pages/index', $data);
         }
-        public function about($id){
-            echo 'This is about ' . $id;
+        public function about(){
+            $data = [
+                'title' => 'About Us'
+            ];
+            $this->view('pages/about', $data);
         }
     }
