9d33e9369f3e977a3d00b7aa98bbc983ce5f469b
KostasEreksonas <k.ereksonas@gmail.com>
2024-07-23 12:33:36 +0300

    Implemented a basic router

--- public/index.php
+++ public/index.php
@@ -5,10 +5,23 @@
  */
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode('/', $path);
 
-$action = $segments[2];
-$controller = $segments[1];
+require "src/router.php";
+
+$router = new Router;
+
+$router->add("/home/index", ["controller" => "home", "action" => "index"]);
+$router->add("/products", ["controller" => "products", "action" => "index"]);
+$router->add("/", ["controller" => "home", "action" => "index"]);
+
+$params = $router->match($path);
+
+if ($params === false) {
+    exit("No route matched");
+}
+
+$action = $params["action"];
+$controller = $params["controller"];
 
 require "src/controllers/$controller.php";
 
