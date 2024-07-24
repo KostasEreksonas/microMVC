e7508d8d08e1a45f0c84b1d2358ae1fee01abf3b
KostasEreksonas <k.ereksonas@gmail.com>
2024-07-24 15:40:47 +0300

    Implemented routing with regex

--- public/src/Framework/Router.php
+++ public/src/Framework/Router.php
@@ -16,11 +16,20 @@ class Router
 
     public function match(string $path)
     {
+        $pattern = "#^/(?<controller>[a-z]+)/(?<action>[a-z]+)$#";
+
+        if (preg_match($pattern, $path, $matches)) {
+            $matches = array_filter($matches, "is_string", ARRAY_FILTER_USE_KEY); // Remove any elements of matches array that doesn't have a string for an index
+            return $matches;
+        }
+        /*
         foreach ($this->routes as $route) {
             if ($route['path'] === $path) {
                 return $route["params"];
             }
         }
+        */
+
         return false;
     }
 }
